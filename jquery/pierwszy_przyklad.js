268556254e294a9eb6b1391a2a57ae0a73e33515
ForcedBucket122 <dzejkob2112@gmail.com>
2024-09-05 15:39:36 +0200

    notatki

---
+++ jquery/pierwszy_przyklad.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+    // kod skryptu
+// });
+    // skrócna forma metody .ready()
+$(function(){
+    $('#page').css('background-color','#00FFFF');
+    $('h2').css('letter-spacing','5px');
+    $('.link').css({
+        'color':'black',
+        'text-decoration':'none',
+    });
+    $('[href="#"]').attr('href','https://zs6sobieski.pl/');
+});
